5cbd8d975f07f6dbb76808b8e296b9d41d230b22
William Darian <williamdariansutedjo@gmail.com>
2024-08-11 15:43:01 +0900

    feat: tune populate logic

--- app/src/cmd_populate.rs
+++ app/src/cmd_populate.rs
@@ -12,17 +12,13 @@ pub async fn populate(input: String, db: &DatabaseConnection) -> Result<()> {
   let data = serde_json::from_str::<Data>(&data)?;
   let data: Vec<administrative_area::ActiveModel> = data
     .into_models()
-    .iter()
-    .map(|model| model.clone().into_active_model())
+    .into_iter()
+    .map(|model| model.into_active_model())
     .collect();
 
   administrative_area::Entity::delete_many().exec(db).await?;
-  // administrative_area::Entity::insert_many(data)
-  //   .on_conflict_do_nothing()
-  //   .exec(db)
-  //   .await?;
 
-  for datum in data.chunks(10) {
+  for datum in data.chunks(64) {
     administrative_area::Entity::insert_many(datum.to_vec())
       .on_conflict_do_nothing()
       .exec(db)
